895a1ae50b86e1736a7168dcb3c9147a17393c78
Wesley S <159206603+WJSmith09@users.noreply.github.com>
2026-05-11 00:35:38 +0000

    finished I2C_module.py

--- project/main.py
+++ project/main.py
@@ -6,7 +6,7 @@ import uos
 from machine import Pin
 
 # File name of the script to import
-file_name = "ultrasonic_sensor"
+file_name = "I2C_module"
 
 SCRIPT_DIRECTORY = "/py_scripts"
 CONTEXT_RADIUS = 2
